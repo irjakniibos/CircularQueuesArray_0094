6452ecbae66705a7cd7f0a0c0d1508ec7f172ded
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-05-31 14:38:47 +0700

    mengecek posisi rear

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -31,5 +31,13 @@ class Queues{
             FRONT = 0;
             REAR = 0;
         }
+        else
+        {
+        //jika REAR berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
     }
 };
