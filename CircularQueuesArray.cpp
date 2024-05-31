c54599277610433d1bc5c5fffe15492db86b88df
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-05-31 15:01:18 +0700

    membuat function Display

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -65,6 +65,48 @@ class Queues{
         }
     }
 
+    void display(){
+        int FRONT_position = FRONT;
+        int REAR_position = REAR;
+
+        //cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue is empty\n";
+            return;
+        }
+        
+        cout << "\nElements in the queue are...\n";
+
+        //Jika FRONT_position <= REAR_position,iterasi dari FONT hingga REAR
+        if(FRONT_position <= REAR_position){
+            while (FRONT_position <= REAR_position){
+                cout << queue_array[FRONT_position]<<"   ";
+                FRONT_position++;
+            }
+        cout << endl;
+        }
+        else
+        {
+            //jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+            while (FRONT_position <= max - 1)
+            {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+
+            FRONT_position = 0;
+
+            //iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+
+    }
 
 
 
